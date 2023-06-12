7c227255e560f882a9afad7c61774df940ad0ded
dojo2023-inagaki <130334502+dojo2023-inagaki@users.noreply.github.com>
2023-06-12 16:26:51 +0900

    変更分

--- src/src/DAO/ReactionsDao.java
+++ src/src/DAO/ReactionsDao.java
@@ -1,6 +1,12 @@
 package DAO;
 
+import java.sql.Connection;
+
+
+
 public class ReactionsDao {
-	
+	public boolean insert(REACTIONS card) {
+		Connection conn = null;
+		boolean result = false;
 
 }
