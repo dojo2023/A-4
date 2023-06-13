4d191cb37bfa796290489555bb58f372c71ec790
dojo2023-inagaki <130334502+dojo2023-inagaki@users.noreply.github.com>
2023-06-13 13:09:42 +0900

    ReactionsDao 一旦完成

--- src/src/DAO/ReactionsDao.java
+++ src/src/DAO/ReactionsDao.java
@@ -2,6 +2,7 @@ package DAO;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.List;
 import model.Reactions;
 
 public class ReactionsDao {
-	public boolean Reactionadd(Reactions reaction) {
+	public boolean Reactioninsert(Reactions reaction) {
 		Connection conn = null;
 		boolean result = false;
 		
@@ -49,7 +50,7 @@ public class ReactionsDao {
 		return result;
 		
 	}
-	public List<Reactions> reactionShow() {
+	public List<Reactions> Reactionselect() {
 		Connection conn = null;
 		List<Reactions> reactionList = new ArrayList<Reactions>(); //Postsのオブジェクトを格納する用のリスト
 
@@ -61,20 +62,27 @@ public class ReactionsDao {
 			String sql = "SELECT SUM (GOOD) WHERE POST_ID = ?";
 					
 			PreparedStatement pStmt = conn.prepareStatement(sql);
-			
-			pStmt.setInt(GOOD);
-			
-			
+			ResultSet rs = pStmt.executeQuery();
+
+			while (rs.next()) {
+				Reactions reaction = new Reactions(
+				
+				rs.getString("REACTION_ID"),
+				rs.getInt("GOOD"),
+				rs.getString("POST_ID")
+				);
+				reactionList.add(reaction);
+			}
 		}
 
 		catch (SQLException e) {
 			e.printStackTrace();
-			postList = null;
+			reactionList = null;
 		}
 
 		catch (ClassNotFoundException e) {
 			e.printStackTrace();
-			postList = null;
+			reactionList = null;
 		}
 
 		finally {
@@ -84,12 +92,52 @@ public class ReactionsDao {
 				}
 				catch (SQLException e) {
 					e.printStackTrace();
-					postList = null;
+					reactionList = null;
 				}
 			}
 		}
 
-		return postList;
+		return reactionList;
+	}
+	public boolean Reactionupdate(Reactions reaction) {
+		Connection conn = null;
+		boolean result = false;
+		
+		try {
+			Class.forName("org.h2.Driver");
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/nyastar", "sa", "");
+			String sql = "UPDATE REACTIONS SET GOOD=? REACTIONTIME=? WHERE REACTION_ID=?";
+			
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+		
+			pStmt.setString(1, reaction.getReaction_id());	
+			pStmt.setInt(2, reaction.getGood());
+			pStmt.setTimestamp(3, reaction.getReaction_time());
+			pStmt.setString(4, reaction.getPost_id());
+			
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return result;
+		
 	}
 }
 	
