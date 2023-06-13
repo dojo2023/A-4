7937919caa4a18b8e6a0c481c4c0cebf6bc878ea
dojo2023-hashimoto-nagisa <hashimotonagisaplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-13 10:46:41 +0900

    Update Progress.java

--- src/src/model/Progress.java
+++ src/src/model/Progress.java
@@ -1,5 +1,18 @@
 package model;
 
-public class Progress {
+import java.io.FileOutputStream;
+import java.io.IOException;
 
-}
+	public class Progress {
+	    public void saveUserIcon(byte[] iconData, String fileName) {
+	        try (FileOutputStream outputStream = new FileOutputStream(fileName)) {
+	            outputStream.write(iconData);
+
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+
+	    }
+
+	}
+
