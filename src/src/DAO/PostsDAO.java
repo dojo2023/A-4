3916eebea415ab9a5eb11bbc0fd3902540bde32b
髙橋碧生｜Aoi Takahashi <takahashiaoiplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-13 10:53:24 +0900

    ユーザ投稿取得、編集、削除メソッドを追加

--- src/src/DAO/PostsDAO.java
+++ src/src/DAO/PostsDAO.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import model.Bc;
 import model.Posts;
 
 public class PostsDAO {
@@ -53,7 +52,7 @@ public class PostsDAO {
 		return result;
 	}
 
-	//全ての投稿を取得
+	//全ての投稿を取得する(タイムライン)
 	public List<Posts> postShow() {
 		Connection conn = null;
 		List<Posts> postList = new ArrayList<Posts>(); //Postsのオブジェクトを格納する用のリスト
@@ -72,16 +71,16 @@ public class PostsDAO {
 			ResultSet rs = pStmt.executeQuery();
 
 			while (rs.next()) {
-				Posts card = new Posts(
+				Posts post = new Posts(
 				rs.getString("POST_ID"),
 				rs.getString("POST_USER_NAME"),
 				rs.getString("POST_MESSAGE"),
 				rs.getInt("GANPARI_TIME"),
 				rs.getString("GENRE_TAG"),
 				rs.getString("GOAL_NAME"),
-				rs.getString("POST_TIME")
+				rs.getTimestamp("POST_TIME")
 				);
-				postList.add(card);
+				postList.add(post);
 			}
 		}
 
@@ -110,39 +109,87 @@ public class PostsDAO {
 		return postList;
 	}
 
-	// 名刺データを更新するメソッド
-	// 引数postで指定されたレコードを更新し、成功したらtrueを返す
-	public boolean update(Bc post) {
+	//特定のユーザの投稿を取得する(ユーザページ)
+	public List<Posts> postShowUser() {
+		Connection conn = null;
+		List<Posts> postList = new ArrayList<Posts>(); //Postsのオブジェクトを格納する用のリスト
+
+		try {
+			Class.forName("org.h2.Driver");
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/simpleBC", "sa", "");
+
+			//
+			String sql = "SELECT POST_ID, USER_NAME, POST_MESSAGE, GANPARI_TIME, GENRE_TAG, GOAL_NAME, POST_TIME"
+					+ "FROM POSTS"
+					+ "JOIN ACCOUNTS ON POSTS.USER_UUID = ACCOUNTS.USER_UUID"
+					+ "JOIN GOALS ON POSTS.USER_UUID = GOALS.USER_UUID"
+					+ "WHERE POST.USER_UUID=?" //ユーザIDを指定する
+					+ "ORDER BY POST_TIME;";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			ResultSet rs = pStmt.executeQuery();
+
+			while (rs.next()) {
+				Posts post = new Posts(
+				rs.getString("POST_ID"),
+				rs.getString("POST_USER_NAME"),
+				rs.getString("POST_MESSAGE"),
+				rs.getInt("GANPARI_TIME"),
+				rs.getString("GENRE_TAG"),
+				rs.getString("GOAL_NAME"),
+				rs.getTimestamp("POST_TIME")
+				);
+				postList.add(post);
+			}
+		}
+
+		catch (SQLException e) {
+			e.printStackTrace();
+			postList = null;
+		}
+
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			postList = null;
+		}
+
+		finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+					postList = null;
+				}
+			}
+		}
+
+		return postList;
+	}
+
+	// 投稿情報を更新する
+	public boolean update(Posts post) {
 		Connection conn = null;
 		boolean result = false;
 
 		try {
 			Class.forName("org.h2.Driver");
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/simpleBC", "sa", "");
-			String sql = "UPDATE BC SET NAME=?, COMPANY=?, DEPARTMENT=?, POSITION=?, ADDRESS=?, EMAIL=?, PHONE_NUMBER=?, POST_CODE=?, MEMO=? WHERE ID = ?";
+			String sql = "UPDATE POSTS SET POST_MESSAGE=?, GANBARI_TIME=?, GOAL_ID=? WHERE POST_ID=?)";
+
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
-			pStmt.setString(1, post.getName());
-			pStmt.setString(2, post.getCompany());
-			pStmt.setString(3, post.getDepartment());
-			pStmt.setString(4, post.getPosition());
-			pStmt.setString(5, post.getAddress());
-			pStmt.setString(6, post.getEmail());
-			pStmt.setString(7, post.getPhone_number());
-			pStmt.setString(8, post.getPost_code());
-			pStmt.setString(9, post.getMemo());
-
-			pStmt.setString(10, post.getId());
-
+			pStmt.setString(1, post.getMsg());
+			pStmt.setInt(2, post.getGanbariTime());
+			pStmt.setString(3, post.getGoalId());
+			pStmt.setString(4, post.getId());
 
 			try {
-			    // 更新処理の実行
 				if (pStmt.executeUpdate() == 1) {
 					result = true;
 				}
 			} catch (Exception e) {
 			    e.printStackTrace();
-			    // 例外の処理またはエラーメッセージの表示など
 			}
 		}
 		catch (SQLException e) {
@@ -152,7 +199,6 @@ public class PostsDAO {
 			e.printStackTrace();
 		}
 		finally {
-			// データベースを切断
 			if (conn != null) {
 				try {
 					conn.close();
@@ -162,13 +208,10 @@ public class PostsDAO {
 				}
 			}
 		}
-
-		// 結果を返す
 		return result;
 	}
 
-	//　名刺データを削除するメソッド
-	// 引数numberで指定されたレコードを削除し、成功したらtrueを返す
+	// 投稿を削除する
 	public boolean delete(String id) {
 		Connection conn = null;
 		boolean result = false;
@@ -177,11 +220,10 @@ public class PostsDAO {
 			Class.forName("org.h2.Driver");
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/simpleBC", "sa", "");
 
-			String sql = "delete from BC where ID=?";
+			String sql = "DELETE FROM POSTS WHERE POST_ID=?";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 			pStmt.setString(1, id);
 
-			// SQL文を実行する
 			if (pStmt.executeUpdate() == 1) {
 				result = true;
 			}
@@ -193,7 +235,6 @@ public class PostsDAO {
 			e.printStackTrace();
 		}
 		finally {
-			// データベースを切断
 			if (conn != null) {
 				try {
 					conn.close();
@@ -203,8 +244,6 @@ public class PostsDAO {
 				}
 			}
 		}
-
-		// 結果を返す
 		return result;
 	}
 }
