a57445d48d090584158ba9239eda5ee0817328f5
dojo2023-hiraiyuina <hiraiyuinaplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-13 13:38:16 +0900

    時間変換

--- src/src/model/timeConverter.java
+++ src/src/model/timeConverter.java
@@ -1,22 +1,30 @@
 package model;
-import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
+
 //【１時間半→９０分に変換するメソッド ＜h時間＊６０＋ｍ分＝分】＞
-//【９０分を→１時間半に変換するメソッド ＜m分/60=h時間  h時間＋%（m分）＝時間分】＞
-
-
 public class TimeConverter {
-int h,m,hans,mans;
-
 	public static void timeConverter(String[] args){
-	Scanner scn = new Scanner(System.in);
+int hours;
+long minutes;
 		  //h時間をm分に変換
-		  System.out.println(TimeUnit.HOURS.toMinutes(h));
+	    hours = 2; // 変換する時間（単位：時間
+	    minutes = TimeUnit.HOURS.toMinutes(hours); // 時間を分に変換
+		System.out.println(hours + " 時間は " + minutes + " 分です。");
+	}
 
-		//m分をh時間 ＋ m分に変換
-		  System.out.println(TimeUnit.MINUTES.toHours(m));
 
-	  }
+	//【９０分を→１時間半に変換するメソッド ＜m分/60=h時間  h時間＋%（m分）＝時間分】＞
+    public static void main(String[] args) {
+ long minutes,hours,remainingMinutes;
+         minutes = 135; // 変換する分（単位：分）
+         hours = TimeUnit.MINUTES.toHours(minutes); // 分を時間に変換
+         remainingMinutes = minutes % 60; // 分の残りを計算
 
+        System.out.println(minutes + " 分は " + hours + " 時間 " + remainingMinutes + " 分です。");
+    }
 }
+
+
+
+
