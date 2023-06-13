5901a3d142759f2f085055113e34e7ec060fa30e
dojo2023-inagaki <130334502+dojo2023-inagaki@users.noreply.github.com>
2023-06-13 10:56:47 +0900

    Update ReactionsDao.java

--- src/src/DAO/ReactionsDao.java
+++ src/src/DAO/ReactionsDao.java
@@ -1,65 +1,34 @@
 package DAO;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import model.Reactions;
 
 public class ReactionsDao {
-	public boolean insert(Reactions card) {
+	public boolean Reactionadd(Reactions reaction) {
 		Connection conn = null;
 		boolean result = false;
 		
 		try {
-			// JDBCドライバを読み込む
 			Class.forName("org.h2.Driver");
-
-			// データベースに接続する
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/nyastar", "sa", "");
-
-			// SQL文を準備する
 			String sql = "insert into REACTIONS values (?, ?, ?, ?, ?)";
+			
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 		
-		if (card.getReaction_id() != null && !card.getReaction_id().equals("")) {
-			pStmt.setString(1, card.getReaction_id());
+			pStmt.setString(1, reaction.getReaction_id());	
+			pStmt.setInt(2, reaction.getGood());
+			pStmt.setString(3, reaction.getPost_id());
+			pStmt.setTimestamp(4, reaction.getReaction_time());
+			pStmt.setString(5, reaction.getUser_uuid());
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
 		}
-		else {
-			pStmt.setString(1, null);
-		}
-		if (card.getGood() != null && !card.getGood()=="") {
-			pStmt.setInt(2, card.getGood());
-		}
-		else {
-			pStmt.setInt(2, null);
-		}
-		if (card.getDepartment() != null && !card.getDepartment().equals("")) {
-			pStmt.setString(3, card.getDepartment());
-		}
-		else {
-			pStmt.setString(3, null);
-		}
-		if (card.getPosition() != null && !card.getPosition().equals("")) {
-			pStmt.setString(4, card.getPosition());
-		}
-		else {
-			pStmt.setString(4, null);
-		}
-		if (card.getName() != null && !card.getName().equals("")) {
-			pStmt.setString(5, card.getName());
-		}
-		else {
-			pStmt.setString(5, null);
-		}
-		// SQL文を実行する
-					if (pStmt.executeUpdate() == 1) {
-						result = true;
-					}
-					
-		
-		
 		catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -67,7 +36,6 @@ public class ReactionsDao {
 			e.printStackTrace();
 		}
 		finally {
-			// データベースを切断
 			if (conn != null) {
 				try {
 					conn.close();
@@ -78,11 +46,53 @@ public class ReactionsDao {
 			}
 		}
 
-		// 結果を返す
 		return result;
+		
 	}
+	public List<Reactions> reactionShow() {
+		Connection conn = null;
+		List<Reactions> reactionList = new ArrayList<Reactions>(); //Postsのオブジェクトを格納する用のリスト
 
+		try {
+			Class.forName("org.h2.Driver");
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/simpleBC", "sa", "");
 
+			//
+			String sql = "SELECT SUM (GOOD) WHERE POST_ID = ?";
+					
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			
+			pStmt.setInt(GOOD);
+			
+			
+		}
+
+		catch (SQLException e) {
+			e.printStackTrace();
+			postList = null;
+		}
+
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			postList = null;
+		}
+
+		finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+					postList = null;
+				}
+			}
+		}
+
+		return postList;
+	}
 }
+	
+
 
 
