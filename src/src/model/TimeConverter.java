50b0033b45e578b127a8c45abf862da217058004
dojo2023-hiraiyuina <hiraiyuinaplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-13 13:04:43 +0900

    タイム変換

--- src/src/model/TimeConverter.java
+++ src/src/model/TimeConverter.java
@@ -1,5 +1,22 @@
 package model;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
-public class timeConverter {
+//【１時間半→９０分に変換するメソッド ＜h時間＊６０＋ｍ分＝分】＞
+//【９０分を→１時間半に変換するメソッド ＜m分/60=h時間  h時間＋%（m分）＝時間分】＞
+
+
+public class TimeConverter {
+int h,m,hans,mans;
+
+	public static void timeConverter(String[] args){
+	Scanner scn = new Scanner(System.in);
+		  //h時間をm分に変換
+		  System.out.println(TimeUnit.HOURS.toMinutes(h));
+
+		//m分をh時間 ＋ m分に変換
+		  System.out.println(TimeUnit.MINUTES.toHours(m));
+
+	  }
 
 }
