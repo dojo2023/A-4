834f604abe10256b8e37002f2c7b2becff13bef6
髙橋碧生｜Aoi Takahashi <takahashiaoiplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-15 13:45:20 +0900

    目標DAO,Beans修正

--- src/src/model/Goals.java
+++ src/src/model/Goals.java
@@ -1,5 +1,9 @@
 package model;
 
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.UUID;
+
 //目標情報(goal_id,goal_name,genre_tag,goal_time,goal_date,user_uuid)を保持するBeans
 
 	public class Goals {
@@ -7,18 +11,38 @@ package model;
 	    private String goalName;
 	    private String genreTag;
 	    private String goalTime;
-	    private String goalDate;
+	    private Timestamp goalDate;
 	    private String userUuid;
 
-	    // コンストラクタ
-	    public Goals(String goalId, String goalName, String genreTag, String goalTime, String goalDate, String userUuid) {
-	        this.goalId = goalId;
+	    // コンストラクタ（登録用）
+	    public Goals(String goalName, String genreTag, String goalTime, String userUuid) {
+	    	// 投稿のUUIDを生成
+			UUID uuid = UUID.randomUUID();
+			String uuidString = uuid.toString();
+
+			// 現在時刻を取得する
+			Date now = new Date();
+			Timestamp ts = new Timestamp(now.getTime());
+
+	        this.goalId = uuidString;
 	        this.goalName = goalName;
 	        this.genreTag = genreTag;
-	        this.genreTag = goalTime;
-	        this.genreTag = goalDate;
-	        this.genreTag = userUuid;
+	        this.goalTime = goalTime;
+	        this.userUuid = userUuid;
+			this.goalDate = ts;
 	    }
+
+	    // コンストラクタ（取得用）
+	    public Goals(String goalId, String goalName, String genreTag, String goalTime, Timestamp goalDate, String userUuid) {
+	    	this.goalId = goalId;
+	        this.goalName = goalName;
+	        this.genreTag = genreTag;
+	        this.goalTime = goalTime;
+	        this.userUuid = userUuid;
+			this.goalDate = goalDate;
+	    }
+
+
 	    // ゲッターメソッド
 	    public String getGoalId() {
 	        return goalId;
@@ -32,7 +56,7 @@ package model;
 	    public String getGoalTime() {
 	        return goalTime;
 	    }
-	    public String getGoalDate() {
+	    public Timestamp getGoalDate() {
 	        return goalDate;
 	    }
 	    public String getUserUuid() {
@@ -40,9 +64,6 @@ package model;
 	    }
 
     // セッターメソッド（必要に応じて追加）
-	    public void setGoal_id(String goalId) {
-	        this.goalId = goalId;
-	    }
 	    public void setGoal_name(String goalName) {
 	        this.goalName = goalName;
 	    }
@@ -52,13 +73,6 @@ package model;
 	    public void setGoalTime(String goalTime) {
 	        this.goalTime = goalTime;
 	    }
-	    public void setGoalDate(String goalDate) {
-	        this.goalDate = goalDate;
-	    }
-	    public void setUserUuid(String userUuid) {
-	        this.userUuid = userUuid;
-	    }
-
 	}
 
 
