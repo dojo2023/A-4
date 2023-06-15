2820a86161472cbb4f847dbf36f40ef1791fa931
dojo2023-teraoka <130334556+dojo2023-teraoka@users.noreply.github.com>
2023-06-15 13:07:34 +0900

    Update UserPage.java

--- src/src/servlet/UserPage.java
+++ src/src/servlet/UserPage.java
@@ -1,5 +1,37 @@
 package servlet;
 
-public class UserPage {
+import java.io.IOException;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import DAO.AccountsDao;
+import DAO.GoalsDao;
+import DAO.PostsDAO;
+import DAO.CommentsDao;
+import model.Comments;
+import model.User;
+import model.Goals;
+import model.Posts;
+
+@WebServlet("/UserPage")
+public class UserPage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if ((String)session.getAttribute("id") == null) {
+			response.sendRedirect("/NYASTER/Login");
+			return;
+		}
+	}
 }
