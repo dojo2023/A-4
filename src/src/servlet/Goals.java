898194722f1f6aa27bcbee8c8eb453176ee0285d
dojo2023-inagaki <130334502+dojo2023-inagaki@users.noreply.github.com>
2023-06-16 13:38:28 +0900

    ランキング機能

--- src/src/servlet/Goals.java
+++ src/src/servlet/Goals.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
