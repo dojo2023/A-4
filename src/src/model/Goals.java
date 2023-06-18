bb5c53f9205029122c0e2b623f6d974688f2b549
髙橋碧生｜Aoi Takahashi <takahashiaoiplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-19 03:43:02 +0900

    目標の進捗時間を持つフィールドを追加

--- src/src/model/Goals.java
+++ src/src/model/Goals.java
@@ -7,12 +7,13 @@ import java.util.UUID;
 //目標情報(goal_id,goal_name,genre_tag,goal_time,goal_date,user_uuid)を保持するBeans
 
 public class Goals {
-    private String goalId;
-    private String goalName;
-    private String genreTag;
-    private int goalTime;
-    private Timestamp goalDate;
-    private String userUuid;
+    private String goalId; //目標UUID
+    private String goalName; //目標名
+    private String genreTag; //タグ
+    private int progress; // 目標時間（達成済みの時間）
+    private int goalTime; // 目標時間
+    private Timestamp goalDate; //目標を追加した時間
+    private String userUuid; //目標を設定したユーザのUUID
 
     // コンストラクタ（登録用）
     public Goals(String goalName, String genreTag, int goalTime, String userUuid) {
@@ -33,11 +34,12 @@ public class Goals {
     }
 
     // コンストラクタ（取得用）
-    public Goals(String goalId, String goalName, String genreTag, int goalTime, Timestamp goalDate, String userUuid) {
+    public Goals(String goalId, String goalName, String genreTag, int goalTime, int progress, Timestamp goalDate, String userUuid) {
     	this.goalId = goalId;
         this.goalName = goalName;
         this.genreTag = genreTag;
         this.goalTime = goalTime;
+        this.progress = progress;
         this.userUuid = userUuid;
 		this.goalDate = goalDate;
     }
@@ -56,6 +58,9 @@ public class Goals {
     public int getGoalTime() {
         return goalTime;
     }
+    public int progress() {
+        return progress;
+    }
     public Timestamp getGoalDate() {
         return goalDate;
     }
@@ -72,6 +77,9 @@ public class Goals {
     }
     public void setGoalTime(int goalTime) {
         this.goalTime = goalTime;
+    }
+    public void setProgress(int progress) {
+        this.progress = progress;
     }
 }
 
