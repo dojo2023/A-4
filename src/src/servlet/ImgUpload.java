9d13e9f0f2656648428bb2cce6983275f43dd1aa
髙橋碧生｜Aoi Takahashi <takahashiaoiplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-19 03:02:30 +0900

    画像アップロード用サーブレット

---
+++ src/src/servlet/ImgUpload.java
@@ -0,0 +1,97 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
+
+import DAO.AccountsDao;
+
+/**
+ * Servlet implementation class ImgUpload
+ */
+@WebServlet("/ImgUpload")
+@MultipartConfig(
+	    fileSizeThreshold = 1024 * 1024, // 1MB
+	    maxFileSize = 1024 * 1024 * 10, // 10MB
+	    maxRequestSize = 1024 * 1024 * 50 // 50MB
+	)
+public class ImgUpload extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException {
+		HttpSession session = request.getSession();
+
+		//セッションスコープにユーザIDが存在しない場合はログインページにリダイレクトする
+		if ((String)session.getAttribute("id") == null) {
+			response.sendRedirect("/NYASTER/Login");
+			return;
+		}
+
+    	RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/imgUploader.jsp");
+		dispatcher.forward(request, response);
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		String userUuid = (String)session.getAttribute("id");
+
+        Part filePart = request.getPart("file");
+        String fileName = getFileName(filePart);
+
+        if (fileName != null && !fileName.isEmpty()) {
+            String extension = "png"; // 保存する拡張子を指定
+
+            // ユニークなファイル名を生成
+            String saveFileName = generateFileName(userUuid, extension);
+
+            // ファイルを保存するディレクトリのパスを指定
+            String uploadDirectory = "C:/pleiades/workspace/data/icon_img/";
+
+            // ファイルを保存するパスを組み立て
+            Path filePath = Path.of(uploadDirectory, saveFileName);
+
+            try (InputStream fileContent = filePart.getInputStream()) {
+                // ファイルをPNG形式で保存
+                Files.copy(fileContent, filePath, StandardCopyOption.REPLACE_EXISTING);
+                System.out.println("ファイルが正常に保存されました。");
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("ファイルの保存に失敗しました。");
+                response.getWriter().println("ファイルの保存に失敗しました。");
+            }
+        }
+        response.sendRedirect("/NYASTER/TopPage"); //元のページにリダイレクト
+	}
+
+    private String generateFileName(String userUuid, String extension) {
+    	AccountsDao aDao = new AccountsDao();
+		model.User loginUser = aDao.showUser(userUuid);
+		String userId = loginUser.getUser_id();
+        return userId + extension;
+    }
+
+    private String getFileName(Part part) {
+        String contentDisposition = part.getHeader("content-disposition");
+        String[] elements = contentDisposition.split(";");
+
+        for (String element : elements) {
+            if (element.trim().startsWith("filename")) {
+                return element.substring(element.indexOf('=') + 1).trim().replace("\"", "");
+            }
+        }
+
+        return null;
+    }
+}
