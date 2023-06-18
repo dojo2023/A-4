359742f36e8990caacc3cdd80675a65edf5bf783
髙橋碧生｜Aoi Takahashi <takahashiaoiplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-19 03:19:41 +0900

    インデント調整

--- src/src/model/PwHashed.java
+++ src/src/model/PwHashed.java
@@ -4,31 +4,30 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class PwHashed {
-	  public static String hashPassword(String password) {
-	        try {
-	            // ハッシュ関数のインスタンスを取得
-	            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+    public static String hashPassword(String password) {
+        try {
+            // ハッシュ関数のインスタンスを取得
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
 
-	            // パスワードをバイト配列に変換してハッシュ計算
-	            byte[] hashedBytes = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+            // パスワードをバイト配列に変換してハッシュ計算
+            byte[] hashedBytes = digest.digest(password.getBytes(StandardCharsets.UTF_8));
 
-	            // バイト配列を16進数文字列に変換
-	            StringBuilder hexString = new StringBuilder();
-	            for (byte b : hashedBytes) {
-	                String hex = Integer.toHexString(0xff & b);
-	                if (hex.length() == 1) {
-	                    hexString.append('0');
-	                }
-	                hexString.append(hex);
-	            }
+            // バイト配列を16進数文字列に変換
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hashedBytes) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
 
-	            // ハッシュ化されたパスワードを返す
-	            return hexString.toString();
-	        } catch (NoSuchAlgorithmException e) {
-	            // ハッシュ関数がサポートされていない場合の例外処理
-	            e.printStackTrace();
-	            return null;
-	        }
-	    }
-	}
-
+            // ハッシュ化されたパスワードを返す
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            // ハッシュ関数がサポートされていない場合の例外処理
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
