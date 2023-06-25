289da486351c3c6a7dfbe82960a9b8ac2caa5f33
髙橋碧生｜Aoi Takahashi <takahashiaoiplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-26 02:03:30 +0900

    不要なコメントを削除

--- src/WebContent/js/asyncComments.js
+++ src/WebContent/js/asyncComments.js
@@ -24,25 +24,21 @@ function asyncComments(id){
 
     console.log("jsonテスト表示");
     for(let i=0; i<data.length; i++){
-
-        // elementText = data[i].user_name;
+        // ユーザ名
         var newElement = document.createElement("div");
         newElement.textContent = data[i].user_name;
-        // newElement.setAttribute("class", "comment-box");
-        //再表示時に一括削除する為の共通クラスを設定
-        newElement.classList.add("comment-contents"); 
+        newElement.classList.add("comment-contents"); //再表示時に一括削除する為の共通クラスを設定
         newElement.classList.add("comment-userName");
         element.insertAdjacentElement("beforeend", newElement);
 
-        // elementText = data[i].comment_content;
+        // コメント本文
         var newElement = document.createElement("div");
         newElement.textContent = data[i].comment_content;
-        // newElement.setAttribute("class", "comment-box");
         newElement.classList.add("comment-contents");
         newElement.classList.add("comment-msg");
         element.insertAdjacentElement("beforeend", newElement);
 
-        // 投稿時間を変換して表示
+        // 投稿時間を変換
         var timestamp = data[i].comment_time; // Javaから受け取ったTimestamp型の変数
         var date = new Date(timestamp);
         var year = date.getFullYear();
