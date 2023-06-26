8da2a08719e56a47763886c22a23ee9097e83d99
dojo2023-hashimoto-nagisa <hashimotonagisaplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-26 16:51:31 +0900

    Update postToast.js

--- src/WebContent/js/postToast.js
+++ src/WebContent/js/postToast.js
@@ -1,14 +1,58 @@
 
 /*トーストCSS.JSの読み込み(JSP内に記述)
 <link rel="stylesheet" type="text/css" href="./css/iziToast.css" media="screen" />
-<script type="text/javascript" src="./js/jquery-3.3.1.js"></script>
-<script type="text/javascript" src="./js/jquery-migrate-3.0.1.js"></script>
+<script type="text/javascript" src="./js/jquery-3.4.1.js"></script>
+<script type="text/javascript" src="./js/jquery-migrate-3.4.1.js"></script>
 <script type="text/javascript" src="./js/iziToast.js"></script>
 <script type="text/javascript" src="./js/postToast.js"></script>
 */
 
 //トーストJS
 
+//成功(緑)のトースト
+  $(function() {
+    $(".login-button").on('click', function(event) {
+      event.preventDefault();
+      iziToast.success({
+        title: '成功',
+        message: '投稿が完了しました',
+      });
+    });
+  })
+
+/*
+
+//失敗(赤)のトースト
+  $(function() {
+    $(".login-button").on('click', function(event) {
+      event.preventDefault();
+      iziToast.error({
+        title: '☆',
+        message: '投稿が完了しました',
+      });
+    });
+  })
+//注意勧告(オレンジ)のトースト
+  $(function() {
+    $(".login-button").on('click', function(event) {
+      event.preventDefault();
+      iziToast.warning({
+        title: '☆',
+        message: '投稿が完了しました',
+      });
+    });
+  })
+//インフォメーション(青)のトースト
+  $(function() {
+    $(".login-button").on('click', function(event) {
+      event.preventDefault();
+      iziToast.info({
+        title: '☆',
+        message: '投稿が完了しました',
+      });
+    });
+  })
+//色なしトースト
   $(function() {
     $(".login-button").on('click', function(event) {
       event.preventDefault();
@@ -19,4 +63,4 @@
     });
   })
 
-
+*/
